e52f7385999527d3bb18d55bffa14bea28f3ad7c
Joshraynz <raynzjosh4@gmail.com>
2026-01-22 15:18:31 -0800

    Official Nexus Website Release

---
+++ env.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    // Replace these with your actual Supabase credentials
+    SUPABASE_URL: "https://tvuhreoctxchdkdquvlt.supabase.co",
+    // FIXED: Removed the space at the start
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2dWhyZW9jdHhjaGRrZHF1dmx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNzI3ODcsImV4cCI6MjA3OTg0ODc4N30.3Zrq1GOhndY7gHQcBF5x2A_nW6CXPbTyO439GopJrYM" 
+};
